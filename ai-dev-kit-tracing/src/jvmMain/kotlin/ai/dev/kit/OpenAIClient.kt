2d01374e91c88218f87e9ab28133373782f85601
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2025-07-29 14:50:24 +0200

    [JBAI-15513] Trace tool calling in OpenAI autologging

--- ai-dev-kit-tracing/src/jvmMain/kotlin/ai/dev/kit/OpenAIClient.kt
+++ ai-dev-kit-tracing/src/jvmMain/kotlin/ai/dev/kit/OpenAIClient.kt
@@ -9,9 +9,6 @@ import io.opentelemetry.semconv.incubating.GenAiIncubatingAttributes.*
 import kotlinx.serialization.json.*
 import okhttp3.HttpUrl
 import okhttp3.Interceptor
-import okhttp3.OkHttpClient
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.Response
 
 @Deprecated("instrument() instead")
 fun createOpenAIClient(): OpenAIClient {
@@ -50,8 +47,27 @@ class OpenTelemetryOpenAILogger : OpenTelemetryOkHttpInterceptor(
 
         body["messages"]?.let {
             for ((index, message) in it.jsonArray.withIndex()) {
-                span.setAttribute("gen_ai.prompt.$index.role", message.jsonObject["role"]?.jsonPrimitive?.content)
+                val role = message.jsonObject["role"]?.jsonPrimitive?.content
+                span.setAttribute("gen_ai.prompt.$index.role", role)
                 span.setAttribute("gen_ai.prompt.$index.content", message.jsonObject["content"]?.toString())
+
+                // when a tool result is encountered
+                if (role?.lowercase() == "tool") {
+                    span.setAttribute("gen_ai.prompt.$index.tool_call_id", message.jsonObject["tool_call_id"]?.jsonPrimitive?.content)
+                }
+            }
+        }
+
+        // See: https://platform.openai.com/docs/api-reference/chat/create
+        body["tools"]?.let {
+            for ((index, tool) in it.jsonArray.withIndex()) {
+                span.setAttribute("gen_ai.tool.$index.type", tool.jsonObject["type"]?.jsonPrimitive?.content)
+                tool.jsonObject["function"]?.jsonObject?.let {
+                    span.setAttribute("gen_ai.tool.$index.name", it["name"]?.jsonPrimitive?.content)
+                    span.setAttribute("gen_ai.tool.$index.description", it["description"]?.jsonPrimitive?.content)
+                    span.setAttribute("gen_ai.tool.$index.parameters", it["parameters"]?.jsonObject?.toString())
+                    span.setAttribute("gen_ai.tool.$index.strict", it["strict"]?.jsonPrimitive?.boolean.toString())
+                }
             }
         }
     }
@@ -65,24 +81,53 @@ class OpenTelemetryOpenAILogger : OpenTelemetryOkHttpInterceptor(
             for ((index, choice) in it.jsonArray.withIndex()) {
                 val index = choice.jsonObject["index"]?.jsonPrimitive?.int ?: index
 
+                span.setAttribute(
+                    "gen_ai.completion.$index.finish_reason",
+                    choice.jsonObject["finish_reason"]?.jsonPrimitive?.content
+                )
+
                 choice.jsonObject["message"]?.jsonObject?.let { message ->
                     span.setAttribute(
                         "gen_ai.completion.$index.role",
                         message.jsonObject["role"]?.jsonPrimitive?.content
                     )
                     span.setAttribute("gen_ai.completion.$index.content", message.jsonObject["content"]?.toString())
-                    // TODO: add tool and function calling
-                    /*
-                    span.setAttribute("gen_ai.completion.$index.tool_calls", message.jsonObject["tool_calls"]?.jsonPrimitive?.content)
-                    span.setAttribute("gen_ai.completion.$index.function_call", message.jsonObject["function_call"]?.jsonPrimitive?.content)
-                    span.setAttribute("gen_ai.completion.$index.annotations", message.jsonObject["annotations"].toString())
-                     */
-                }
 
-                span.setAttribute(
-                    "gen_ai.completion.$index.finish_reason",
-                    choice.jsonObject["finish_reason"]?.jsonPrimitive?.content
-                )
+                    // See: https://platform.openai.com/docs/api-reference/chat/object
+                    message.jsonObject["tool_calls"]?.let {
+                        // sometimes, this prop is explicitly set to null, hence, being JsonNull.
+                        // therefore, we check for the required array type
+                        if (it is JsonArray) {
+                            for ((toolCallIndex, toolCall) in it.jsonArray.withIndex()) {
+                                // gen_ai.tool.call.id
+                                span.setAttribute(
+                                    "gen_ai.completion.$index.tool.$toolCallIndex.call.id",
+                                    toolCall.jsonObject["id"]?.jsonPrimitive?.content
+                                )
+                                // gen_ai.tool.type
+                                span.setAttribute(
+                                    "gen_ai.completion.$index.tool.$toolCallIndex.call.type",
+                                    toolCall.jsonObject["type"]?.jsonPrimitive?.content
+                                )
+
+                                // extract function name and arguments
+                                toolCall.jsonObject["function"]?.jsonObject?.let {
+                                    // gen_ai.tool.name
+                                    span.setAttribute(
+                                        "gen_ai.completion.$index.tool.$toolCallIndex.name",
+                                        it["name"]?.jsonPrimitive?.content
+                                    )
+                                    span.setAttribute(
+                                        "gen_ai.completion.$index.tool.$toolCallIndex.arguments",
+                                        it["arguments"]?.jsonPrimitive?.content
+                                    )
+                                }
+                            }
+                        }
+                    }
+
+                    span.setAttribute("gen_ai.completion.$index.annotations", message.jsonObject["annotations"].toString())
+                }
             }
         }
 
--- ai-dev-kit-tracing/src/jvmTest/kotlin/ai/dev/kit/tracing/fluent/OpenAITracingTest.kt
+++ ai-dev-kit-tracing/src/jvmTest/kotlin/ai/dev/kit/tracing/fluent/OpenAITracingTest.kt
@@ -4,8 +4,16 @@ import ai.dev.kit.instrument
 import ai.dev.kit.tracing.BaseOpenTelemetryTracingTest
 import ai.dev.kit.tracing.LITELLM_URL
 import ai.dev.kit.tracing.autologging.createLiteLLMClient
+import com.openai.core.JsonArray
+import com.openai.core.JsonString
+import com.openai.core.JsonValue
 import com.openai.models.ChatModel
+import com.openai.models.FunctionDefinition
+import com.openai.models.FunctionParameters
+import com.openai.models.chat.completions.ChatCompletion
 import com.openai.models.chat.completions.ChatCompletionCreateParams
+import com.openai.models.chat.completions.ChatCompletionTool
+import com.openai.models.chat.completions.ChatCompletionToolMessageParam
 import io.opentelemetry.api.common.AttributeKey
 import io.opentelemetry.api.trace.StatusCode
 import kotlinx.coroutines.test.runTest
@@ -77,6 +85,96 @@ class OpenAITracingTest : BaseOpenTelemetryTracingTest() {
     }
 
     @Test
+    fun `test OpenAI tool calls auto tracing`() = runTest {
+        val client = instrument(createLiteLLMClient())
+
+        // defines: `greet(name: String)`
+        val greetTool = createGreetTool()
+
+        val params = ChatCompletionCreateParams.Companion.builder()
+            .addUserMessage("Use a given `greet` tool to greet two people: Alex and Aleksandr. You MUST do this with the given tool!")
+            .addTool(greetTool)
+            .model(ChatModel.Companion.GPT_4O_MINI)
+            .temperature(0.0)
+            .build()
+
+        client.chat().completions().create(params)
+
+        val traces = analyzeSpans()
+
+        assertEquals(1, traces.size)
+        val trace = traces.firstOrNull()
+        assertNotNull(trace)
+
+        assertEquals("greet", trace.attributes[AttributeKey.stringKey("gen_ai.tool.0.name")])
+        assertEquals("function", trace.attributes[AttributeKey.stringKey("gen_ai.tool.0.type")])
+        assertTrue(trace.attributes[AttributeKey.stringKey("gen_ai.tool.0.description")]?.isNotEmpty() == true)
+        assertTrue(trace.attributes[AttributeKey.stringKey("gen_ai.tool.0.parameters")]?.isNotEmpty() == true)
+
+        // if AI called the tool when check its props
+        if (trace.attributes[AttributeKey.stringKey("gen_ai.completion.0.finish_reason")] == "tool_calls") {
+            assertTrue(trace.attributes[AttributeKey.stringKey("gen_ai.completion.0.tool.0.name")]?.isNotEmpty() == true)
+            assertTrue(trace.attributes[AttributeKey.stringKey("gen_ai.completion.0.tool.0.call.id")]?.isNotEmpty() == true)
+            assertTrue(trace.attributes[AttributeKey.stringKey("gen_ai.completion.0.tool.0.call.type")]?.isNotEmpty() == true)
+            assertTrue(trace.attributes[AttributeKey.stringKey("gen_ai.completion.0.tool.0.arguments")]?.isNotEmpty() == true)
+        }
+    }
+
+    @Test
+    fun `test OpenAI response to a tool call auto tracing`() = runTest {
+        val client = instrument(createLiteLLMClient())
+
+        // defines: `greet(name: String)`
+        val greetTool = createGreetTool()
+
+        // See example at:
+        // https://github.com/openai/openai-java/blob/main/openai-java-example/src/main/java/com/openai/example/FunctionCallingRawExample.java
+        val paramsBuilder = ChatCompletionCreateParams.Companion.builder()
+            .addUserMessage("Use a given `greet` tool to greet a person Alex. You MUST do this with the given tool!")
+            .addTool(greetTool)
+            .model(ChatModel.Companion.GPT_4O_MINI)
+            .temperature(0.0)
+
+        // expect AI to request a tool call
+        client.chat().completions().create(paramsBuilder.build()).choices().stream()
+            .map(ChatCompletion.Choice::message)
+            .peek(paramsBuilder::addMessage)
+            .flatMap { message -> message.toolCalls().stream().flatMap { it.stream() } }
+            .forEach { toolCall ->
+                // add an answer to a tool call
+                paramsBuilder.addMessage(
+                    ChatCompletionToolMessageParam.builder()
+                        .toolCallId(toolCall.id())
+                        .content("Hello! I'm greeting you!")
+                        .build()
+                )
+            }
+
+        // give an answer to a tool call
+        client.chat().completions().create(paramsBuilder.build())
+
+        val traces = analyzeSpans()
+
+        assertEquals(2, traces.size)
+        // contains AI's request for a tool call
+        val toolCallRequestTrace = traces.firstOrNull()
+        // contains an answer to a tool call
+        val toolCallResponseTrace = traces.lastOrNull()
+        assertNotNull(toolCallRequestTrace)
+        assertNotNull(toolCallResponseTrace)
+
+        assertEquals("greet", toolCallRequestTrace.attributes[AttributeKey.stringKey("gen_ai.tool.0.name")])
+        assertEquals("function", toolCallRequestTrace.attributes[AttributeKey.stringKey("gen_ai.tool.0.type")])
+
+        // if AI called the tool when check its props
+        if (toolCallRequestTrace.attributes[AttributeKey.stringKey("gen_ai.completion.0.finish_reason")] == "tool_calls") {
+            assertEquals("tool", toolCallResponseTrace.attributes[AttributeKey.stringKey("gen_ai.prompt.2.role")])
+            assertTrue(toolCallResponseTrace.attributes[AttributeKey.stringKey("gen_ai.prompt.2.content")]?.isNotEmpty() == true)
+            assertTrue(toolCallResponseTrace.attributes[AttributeKey.stringKey("gen_ai.prompt.2.tool_call_id")]?.isNotEmpty() == true)
+        }
+    }
+
+    @Test
     fun `test OpenAI auto tracing when instrumentation is off`() = runTest {
         val client = createLiteLLMClient()
         val params = ChatCompletionCreateParams.Companion.builder()
@@ -93,5 +191,28 @@ class OpenAITracingTest : BaseOpenTelemetryTracingTest() {
         val content = result.choices().first().message().content().getOrNull()
         assertNotNull(content)
         assertTrue(content.isNotEmpty())
+    }
+
+    private fun createGreetTool(): ChatCompletionTool {
+        return ChatCompletionTool.builder()
+            .type(JsonString.of("function"))
+            .function(
+                FunctionDefinition.builder()
+                    .description("Greet the user")
+                    .name("greet")
+                    .parameters(FunctionParameters.builder()
+                        .putAdditionalProperty("type", JsonValue.from("object"))
+                        .putAdditionalProperty(
+                            "properties", JsonValue.from(mapOf(
+                                "name" to mapOf("type" to "string"),
+                            )))
+                        .putAdditionalProperty("required", JsonArray.of(listOf(
+                            JsonString.of("name")
+                        )))
+                        .putAdditionalProperty("additionalProperties", JsonValue.from(false))
+                        .build()
+                    )
+                    .build()
+            ).build()
     }
 }
