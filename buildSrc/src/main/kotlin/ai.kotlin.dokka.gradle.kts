22f6baf493468193aa03584aeadda3bf22b62bec
Vladislav Artiukhov <47076189+Vladislav0Art@users.noreply.github.com>
2025-11-28 14:46:30 +0100

    JBAI-18348: Dynamically load versions of external documentation pages (#129)
    * feat: dynamically load external docs' versions
    * version: update dokka to 2.1.0

--- buildSrc/src/main/kotlin/ai.kotlin.dokka.gradle.kts
+++ buildSrc/src/main/kotlin/ai.kotlin.dokka.gradle.kts
@@ -1,8 +1,26 @@
+
 plugins {
     id("org.jetbrains.dokka")
 }
 
+private fun Project.versionOf(name: String): String {
+    val catalogs = extensions.getByType(VersionCatalogsExtension::class.java)
+    val libs = catalogs.named("libs")
+    return libs.findVersion(name).get().requiredVersion
+}
+
+private fun Project.getVersions() = object {
+    // NOTE: the actual version used doesn't have a package list published, using latest
+    val okhttp = "latest"
+    val opentelemetry = versionOf("opentelemetry")
+    val openai = versionOf("openai")
+    val gemini = versionOf("gemini")
+    val anthropic = versionOf("anthropic")
+}
+
 dokka {
+    val versions = project.getVersions()
+
     dokkaSourceSets.configureEach {
          includes.from("Module.md")
 
@@ -32,50 +50,50 @@ dokka {
         }
 
         externalDocumentationLinks.register("okhttp3") {
-            url("https://javadoc.io/doc/com.squareup.okhttp3/okhttp/latest/")
-            packageListUrl("https://javadoc.io/doc/com.squareup.okhttp3/okhttp/latest/package-list")
+            url("https://javadoc.io/doc/com.squareup.okhttp3/okhttp/${versions.okhttp}/")
+            packageListUrl("https://javadoc.io/doc/com.squareup.okhttp3/okhttp/${versions.okhttp}/package-list")
         }
 
         // Open Telemetry Documentation
         externalDocumentationLinks.register("opentelemetry-api") {
-            url("https://javadoc.io/doc/io.opentelemetry/opentelemetry-api/latest/")
-            packageListUrl("https://javadoc.io/doc/io.opentelemetry/opentelemetry-api/latest/element-list")
+            url("https://javadoc.io/doc/io.opentelemetry/opentelemetry-api/${versions.opentelemetry}/")
+            packageListUrl("https://javadoc.io/doc/io.opentelemetry/opentelemetry-api/${versions.opentelemetry}/element-list")
         }
 
         externalDocumentationLinks.register("opentelemetry-sdk") {
-            url("https://javadoc.io/doc/io.opentelemetry/opentelemetry-sdk/latest/")
-            packageListUrl("https://javadoc.io/doc/io.opentelemetry/opentelemetry-sdk/latest/element-list")
+            url("https://javadoc.io/doc/io.opentelemetry/opentelemetry-sdk/${versions.opentelemetry}/")
+            packageListUrl("https://javadoc.io/doc/io.opentelemetry/opentelemetry-sdk/${versions.opentelemetry}/element-list")
         }
 
         externalDocumentationLinks.register("opentelemetry-exporter-otlp") {
-            url("https://javadoc.io/doc/io.opentelemetry/opentelemetry-exporter-otlp/latest/")
-            packageListUrl("https://javadoc.io/doc/io.opentelemetry/opentelemetry-exporter-otlp/latest/element-list")
+            url("https://javadoc.io/doc/io.opentelemetry/opentelemetry-exporter-otlp/${versions.opentelemetry}/")
+            packageListUrl("https://javadoc.io/doc/io.opentelemetry/opentelemetry-exporter-otlp/${versions.opentelemetry}/element-list")
         }
 
         externalDocumentationLinks.register("opentelemetry-exporter-logging") {
-            url("https://javadoc.io/doc/io.opentelemetry/opentelemetry-exporter-logging/latest/")
-            packageListUrl("https://javadoc.io/doc/io.opentelemetry/opentelemetry-exporter-logging/latest/element-list")
+            url("https://javadoc.io/doc/io.opentelemetry/opentelemetry-exporter-logging/${versions.opentelemetry}/")
+            packageListUrl("https://javadoc.io/doc/io.opentelemetry/opentelemetry-exporter-logging/${versions.opentelemetry}/element-list")
         }
 
         externalDocumentationLinks.register("opentelemetry-sdk-testing") {
-            url("https://javadoc.io/doc/io.opentelemetry/opentelemetry-sdk-testing/latest/")
-            packageListUrl("https://javadoc.io/doc/io.opentelemetry/opentelemetry-sdk-testing/latest/element-list")
+            url("https://javadoc.io/doc/io.opentelemetry/opentelemetry-sdk-testing/${versions.opentelemetry}/")
+            packageListUrl("https://javadoc.io/doc/io.opentelemetry/opentelemetry-sdk-testing/${versions.opentelemetry}/element-list")
         }
 
         // LLM Providers Documentations
         externalDocumentationLinks.register("openai-java") {
-            url("https://javadoc.io/doc/com.openai/openai-java/latest/")
-            packageListUrl("https://javadoc.io/doc/com.openai/openai-java/latest/element-list")
+            url("https://javadoc.io/doc/com.openai/openai-java/${versions.openai}/")
+            packageListUrl("https://javadoc.io/doc/com.openai/openai-java/${versions.openai}/element-list")
         }
 
         externalDocumentationLinks.register("google-genai") {
-            url("https://javadoc.io/doc/com.google.genai/google-genai/latest/")
-            packageListUrl("https://javadoc.io/doc/com.google.genai/google-genai/latest/element-list")
+            url("https://javadoc.io/doc/com.google.genai/google-genai/${versions.gemini}/")
+            packageListUrl("https://javadoc.io/doc/com.google.genai/google-genai/${versions.gemini}/element-list")
         }
 
-        externalDocumentationLinks.register("com.anthropic:anthropic-java") {
-            url("https://javadoc.io/doc/com.anthropic/anthropic-java/latest/")
-            packageListUrl("https://javadoc.io/doc/com.anthropic/anthropic-java/latest/element-list")
-        }
+//        externalDocumentationLinks.register("com.anthropic:anthropic-java") {
+//            url("https://javadoc.io/doc/com.anthropic/anthropic-java/${versions.anthropic}/")
+//            packageListUrl("https://javadoc.io/doc/com.anthropic/anthropic-java/${versions.anthropic}/element-list")
+//        }
     }
 }
